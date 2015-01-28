e015cf922567cd7d8fc7c76bda444da7fd52ee57
Eric Lin <eric@dhcp-mcfad-1733.eduroam.cornell.edu>
2015-01-27 23:19:57 -0500

    initial commit (complete without sound)

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,31 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Eric Lin on 1/7/15.
+//  Copyright (c) 2015 Eric Lin. All rights reserved.
+//
+
+import Foundation
+
+class Array2D<T>{
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int){
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue){
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
